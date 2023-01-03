c4e1c8d252618ae7cbcba328473cddf191832eae
Omar Gamel <omar.gamel@baianat.com>
2023-01-03 17:48:01 +0200

    feat: add download file endpoint

--- src/upload/uploader.controller.ts
+++ src/upload/uploader.controller.ts
@@ -1,10 +1,24 @@
-import { Bind, Controller, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import {
+  Bind,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  StreamableFile,
+  UploadedFiles,
+  UseInterceptors
+} from '@nestjs/common';
 import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { UploadService } from './upload.service';
 import { FileModelEnum } from './file.enum';
 import { UploadFileInput } from './upload-file.input';
 import { ModelWhichUploadedFor, Upload, UploadedFile } from './upload.type';
+import { Response } from 'express';
+import { join } from 'path';
+import * as fs from 'fs';
+
 @Controller()
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
@@ -22,13 +36,35 @@ export class UploadController {
       file1?: Express.Multer.File[];
       file2?: Express.Multer.File[];
     }
-  )
-  {
+  ) {
     return await this.uploadService.restUpload({
       files,
       saveTo: FileModelEnum.CONSULTANT_ATTACHMENTS,
       modelWhichUploadedFor: null,
       oldFile: null
     });
+  }
+
+  @Get('download/:id')
+  async downloadFile(@Param() params, @Res() res) {
+    const filePath = join(process.cwd(), 'public', params.id);
+    return res.download(filePath);
+  }
+
+  @Get('*/:id')
+  async getFile(@Param() params, @Res() res): Promise<string> {
+    const filePath = join(process.cwd(), 'public', params['0'], params.id);
+    if (!fs.existsSync(filePath)) return res.send('file not found');
+
+    return res.sendFile(filePath);
+  }
+
+  @Get('/:id')
+  async getFiles(@Param() params, @Res() res): Promise<string> {
+    const filePath = join(process.cwd(), 'public', params.id);
+
+    if (!fs.existsSync(filePath)) return res.send('file not found');
+
+    return res.sendFile(filePath);
   }
 }
